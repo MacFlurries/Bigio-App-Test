4bcb231df6707de3230b989af5c11513b1c08509
Riyan <153371196+riyannnnnih@users.noreply.github.com>
2025-07-30 12:10:19 +0700

    fix: update characterRepository & add new folders

--- app/repositories/characterRepository.ts
+++ app/repositories/characterRepository.ts
@@ -20,4 +20,37 @@ export const CharacterRepository = {
     favorites = favorites.filter((c: Character) => c.id !== id);
     await saveFavorites(favorites);
   },
+
+  
+export async function fetchCharacters(): Promise<Character[]> {
+  try {
+    const response = await fetch(`${BASE_URL}/character`);
+    const data = await response.json();
+    return data.results;
+  } catch (err) {
+    console.error("Error fetching characters:", err);
+    return [];
+  }
+}
+
+export async function fetchCharacterById(id: number): Promise<Character | null> {
+  try {
+    const response = await fetch(`${BASE_URL}/character/${id}`);
+    return await response.json();
+  } catch (err) {
+    console.error("Error fetching character:", err);
+    return null;
+  }
+}
+
+export async function searchCharactersByName(name: string): Promise<Character[]> {
+  try {
+    const response = await fetch(`${BASE_URL}/character/?name=${name}`);
+    const data = await response.json();
+    return data.results || [];
+  } catch {
+    return [];
+  }
+}
+
 };
